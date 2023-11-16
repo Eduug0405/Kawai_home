6122da23287da749f5ea2e7945df76cb064e7b8f
Eduug0405 <223233@ids.upchiapas.edu.mx>
2023-11-16 08:47:43 -0600

    get en gestion productos

--- src/componentes/tabla.js
+++ src/componentes/tabla.js
@@ -1,44 +1,51 @@
-
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../estilos/tabla.css';
+import axios from "axios";
 
 export default function Tabla() {
+    const [productos, setProductos] = useState([]);
 
-    const datosDeLaBaseDeDatos = [
-        { nombre: 'Espejo', precio: 50 },
-        { nombre: 'mascarilla', precio: 20 },
-        { nombre: 'ventilador',  precio: 100 },
-        
-      ];
+    useEffect(() => {
 
+        fetchData();
+    }, []);
 
-    const handleEliminar = (id) => () => {
+    const fetchData = async () => {
+        try {
+            const response = await axios.get("http://localhost:3000/productos",);
+            //const productosArray = Object.values(response.data);
+            setProductos(response.data.data);
+            console.log(response)
+        } catch (error) {
+            console.error("Error al obtener datos:", error);
+        }
+    };
+
+    const handleEliminar = (id) => {
         console.log(`Eliminar elemento con ID: ${id}`);
     };
-      
-    const handleModificar = (id) => () => {
+
+    const handleModificar = (id) => {
         console.log(`Modificar elemento con ID: ${id}`);
     };
 
     return (
         <div className="table">
-        <div className="table-row table-header">
-          <div className="table-cell">Nombre</div>
-          <div className="table-cell">Precio</div>
-          <div className="table-cell">Acciones</div>
+            <div className="table-row table-header">
+                <div className="table-cell">Nombre</div>
+                <div className="table-cell">Precio</div>
+                <div className="table-cell">Acciones</div>
+            </div>
+            {productos.map((item) => (
+                <div key={item.id} className="table-row">
+                    <div className="table-cell">{item.nombre}</div>
+                    <div className="table-cell">{item.precio}</div>
+                    <div className="table-cell">
+                        <button className="button-55" onClick={() => handleEliminar(item.id)}>Eliminar</button>
+                        <button className="button-55" onClick={() => handleModificar(item.id)}>Modificar</button>
+                    </div>
+                </div>
+            ))}
         </div>
-        {datosDeLaBaseDeDatos.map((item) => (
-          <div key={item.id} className="table-row">
-            <div className="table-cell">{item.nombre}</div>
-            <div className="table-cell">{item.precio}</div>
-            <div className="table-cell">
-            <button className="button-55" onClick={() => handleEliminar(item.id)}>Eliminar</button>
-            <button className="button-55" onClick={() => handleModificar(item.id)}>Modificar</button>
-          </div>
-          </div>
-        ))}
-      </div>
- 
     );
-  }
-  
+}
