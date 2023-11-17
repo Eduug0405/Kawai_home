7454056a43c341e1c30c784ac9dad98c1ad69e48
Eduug0405 <223233@ids.upchiapas.edu.mx>
2023-11-16 19:42:09 -0600

    get de las ventas

--- Components/Corte.js
+++ Components/Corte.js
@@ -1,47 +1,66 @@
 "use client"
-import React from "react";
+import React, { useState, useEffect } from "react";
 import 'Styles/StylesCorte.css';
 import DataTable from 'react-data-table-component';
-const Corte = () =>{
-   const columns = [
-       {
-         name: 'Nombre',
-           selector: row => row.name
-       },
-       {
-          name: 'Cantidad',
-           selector: row => row.cantidad
-       },
-       {
-         name: "Total",
-         selector:row => row.total
-       }
-   ];
+import axios, { get } from "axios";
 
+const Corte = () => {
 
+    const [ventas, setVentas] = useState([]);
 
-   const data =[
-       {
-           name:'Mochila Grande',
-           cantidad:'2',
-           total:'100'
-       }
-   ]
+    useEffect(() => {
 
-    return(
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        try {
+            const response = await axios.get("http://localhost:3000/ventas");
+            setVentas(response.data.data);
+            console.log(response);
+        } catch (error) {
+            console.log("Error al obtener datos", error);
+        }
+    };
+
+    const columns = [
+        {
+            name: 'Nombre',
+            selector: row => row.nombre
+        },
+        {
+            name: 'Cantidad',
+            selector: row => row.cantidad
+        },
+        {
+            name: "Total",
+            selector: row => row.total
+        }
+    ];
+
+    const data = ventas.map((venta) => {
+        return {
+            nombre: venta.nombre,
+            cantidad: venta.cantidad,
+            total: venta.total
+        };
+    });
+
+    return (
         <div className="cuerpo">
-              <h5>Registro de Ventas</h5>
-        <img className="bambu" src="/assets/bambu.png"/>
-        <DataTable
-            className="custom"
-            columns={columns}
-            data={data}
-        >
-        </DataTable>
+            <h5>Registro de Ventas</h5>
+            <img className="bambu" src="/assets/bambu.png" />
+
+            <DataTable
+                className="custom"
+                columns={columns}
+                data={data}
+            >
+            </DataTable>
             <button className="button type1">
             </button>
         </div>
-    )
-}
+    );
+};
 
 export default Corte;
