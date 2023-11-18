45c7d3ab21409902924fc3e74bb92665ed15252c
Eduug0405 <223233@ids.upchiapas.edu.mx>
2023-11-18 00:04:55 -0600

    MODIFICACION FRONT

--- src/app/components/Ventas.js
+++ src/app/components/Ventas.js
@@ -1,47 +1,24 @@
-"use client"
-import React, {useEffect, useState} from "react";
-import'/Styles/StylesVenta.css';
-import {useRouter} from "next/navigation";
-
-const Ventas = () => {
-    const navigation = useRouter()
-
-
-
-
-    const handleClick = ()=>{
-        navigation.push("/addVentaLink")
-    }
+import React from "react";
+import '../../../Styles/StylesAddVenta.css'
+const Ventas= () =>{
     return(
         <div className="cuerpo">
             <div className="title">
-              <h1>Ventas Realizadas</h1>
-
+                <h1>REGISTRO DE VENTAS</h1>
+            </div>
+            <div className="input-container">
+                <input type="text" placeholder="Buscar Producto"/>
+                <button className="button"></button>
             </div>
 
-
-
-            <div className="btn-venta">
-            <button>Guardar Venta</button>
+            <div className="textInputWrapper">
+                <input placeholder="Cantidad" type="text" className="textInput"/>
             </div>
 
-
-            <button type="button" className="btn cube cube-hover" onClick={handleClick}>
-                <div className="bg-top">
-                    <div className="bg-inner"></div>
-                </div>
-                <div className="bg-right">
-                    <div className="bg-inner"></div>
-                </div>
-                <div className="bg">
-                    <div className="bg-inner"></div>
-                </div>
-                <div className="text">Nueva Venta</div>
-
+            <button className="btn-add">
+                Confirmar
             </button>
-
         </div>
-
     )
 }
 
